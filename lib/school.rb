a152156ce3d0a6140c8ec50efde973ce3bb1a3a9
Oscar Alvarez <gavinalv6@gmail.com>
2020-08-15 16:17:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,7 +11,10 @@ class School
   
   def add_student(name, grade)
     if roster.has_key?(grade)
-    roster[grade] = [name]
+      roster[grade] << name 
+    else 
+      roster[grade] = [name]
+    end 
   end 
     
   
